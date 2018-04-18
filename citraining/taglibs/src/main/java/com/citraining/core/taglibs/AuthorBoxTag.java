9a6ddba98fd1ffead2dc39c4508ab30188daf9c0
Phool Chandra <phoolhbti@gmail.com>
2018-04-18 15:29:28 +0530

    tag lib impl

--- citraining/taglibs/src/main/java/com/citraining/core/taglibs/AuthorBoxTag.java
+++ citraining/taglibs/src/main/java/com/citraining/core/taglibs/AuthorBoxTag.java
@@ -13,35 +13,40 @@ import org.slf4j.LoggerFactory;
 
 import com.day.cq.wcm.api.WCMMode;
 
-public class AuthorBoxTag extends BodyTagSupport{
-	/**
-	 * 
-	 */
+public class AuthorBoxTag extends BodyTagSupport {
+
 	private static final long serialVersionUID = 1L;
-	private static final Logger LOGGER=LoggerFactory.getLogger(AuthorBoxTag.class);
-	private transient PageContext pc=null;
-	public int doAfterBody() throws JspException{
-		try {
-			BodyContent bodyContent=super.getBodyContent();
-			String bodyString=bodyContent.getString();
-			if(WCMMode.fromRequest(pc.getRequest())==WCMMode.EDIT){
-				JspWriter out=bodyContent.getEnclosingWriter();
-				bodyString= "<div style=\"border-width:1px;border-style:solid;border-color:blue\">"
-				+bodyString+"</div>";
-				out.write(bodyString);
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuthorBoxTag.class);
+
+	private transient PageContext aemPageContext = null;
+
+	@Override
+	public int doAfterBody() throws JspException {
+		try{
+			BodyContent bodyContent = super.getBodyContent();
+			String bodyString = bodyContent.getString();
+			JspWriter out = bodyContent.getEnclosingWriter();
+			if (WCMMode.fromRequest(this.aemPageContext.getRequest()) == WCMMode.EDIT){
+				bodyString = "<div style=\"border-width:1px;border-style:solid;border-color:blue\">" + bodyString + "</div>";
+			} else if (WCMMode.fromRequest(this.aemPageContext.getRequest()) == WCMMode.DISABLED){
+				bodyString = "<div>" + bodyString + "</div>";
 			}
-		} catch (IOException e) {
-			LOGGER.error("I/O Error in authorbox tag :",e);
+			out.write(bodyString);
+		} catch (IOException e){
+			LOGGER.error("I/O Error in authorbox tag :{}", e);
 		}
 		return SKIP_BODY;
 	}
+
 	@Override
-	public void setPageContext(PageContext p){
-		pc=p;		
+	public void setPageContext(PageContext pageContext) {
+		this.aemPageContext = pageContext;
 	}
+
 	@Override
-	public void release(){
-		pc=null;
+	public void release() {
+		this.aemPageContext = null;
 	}
 
 }
--- citraining/taglibs/src/main/java/com/citraining/core/taglibs/PageHelper.java
+++ citraining/taglibs/src/main/java/com/citraining/core/taglibs/PageHelper.java
@@ -2,8 +2,9 @@ package com.citraining.core.taglibs;
 
 public class PageHelper {
 
-	public static final String MESSAGE="Hello world by Taglib...";
-	public String getMessage(){
+	public static final String MESSAGE = "Hello world by Taglib...";
+
+	public String getMessage() {
 		return MESSAGE;
 	}
 }
